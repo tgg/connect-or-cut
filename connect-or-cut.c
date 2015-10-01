1d2e548fbf3002c544f7865ab53f8a67fdf6bc17
Thomas Girard <thomas.g.girard@free.fr>
2015-10-01 21:55:49 +0200

    align license in code with LICENSE

--- connect-or-cut.c
+++ connect-or-cut.c
@@ -1,6 +1,6 @@
 /* connect-or-cut -- block unwanted connect() calls.
  *
- * Copyright © 2015, Thomas Girard <thomas.g.girard@free.fr>
+ * Copyright (c) 2015, Thomas Girard <thomas.g.girard@free.fr>
  *
  * All rights reserved.
  *
@@ -8,14 +8,14 @@
  * modification, are permitted provided that the following conditions
  * are met:
  *
- * 1. Redistributions of source code must retain the above copyright
+ *  * Redistributions of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
  *
- * 2. Redistributions in binary form must reproduce the above copyright
+ *  * Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
  *
- * 3. Neither the name of the author nor the names of its contributors
+ *  * Neither the name of the author nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
