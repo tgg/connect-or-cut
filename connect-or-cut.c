66a2977487a86edcf653891a05757f9c98313f73
Thomas Girard <thomas.g.girard@free.fr>
2016-04-12 00:14:10 +0200

    release 1.0.0

--- connect-or-cut.c
+++ connect-or-cut.c
@@ -164,7 +164,7 @@ getprogname ()
 }
 #endif
 
-static const char version[] = "connect-or-cut v1.0.0-SNAPSHOT";
+static const char version[] = "connect-or-cut v1.0.0";
 static volatile bool initialized = false;
 static bool needs_dns_lookup = false;
 static coc_log_level_t log_level = COC_BLOCK_LOG_LEVEL;
