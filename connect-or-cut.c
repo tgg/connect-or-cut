445e57b324b1d2b8471ab60f9fc89e39f9e2f402
Thomas Girard <thomas.g.girard@free.fr>
2016-12-29 21:51:54 +0100

    macro hygiene
    It turns out my first attempt was too Linux centric and failed to compile
    on macOS.
    
    Thank you, Travis CI :-)

--- connect-or-cut.c
+++ connect-or-cut.c
@@ -926,10 +926,9 @@ coc_init (void)
   (a->sa_family == AF_INET ? \
    ((const struct sockaddr_in *) a)->sin_port : ((const struct sockaddr_in6 *) a)->sin6_port)
 
-#define INETX_ADDR(a) \
-  (a->sa_family == AF_INET ? \
-   (void *) &((const struct sockaddr_in *) a)->sin_addr : \
-   (void *) &((const struct sockaddr_in6 *) a)->sin6_addr)
+#define INET4_ADDR(a) (&((const struct sockaddr_in *) a)->sin_addr)
+#define INET6_ADDR(a) (&((const struct sockaddr_in6 *) a)->sin6_addr)
+#define INETX_ADDR(a) (a->sa_family == AF_INET ? (void *) INET4_ADDR (a) : (void *) INET6_ADDR (a))
 
 static inline
 bool
@@ -940,7 +939,7 @@ coc_rule_match (coc_entry_t *e, const struct sockaddr *addr, const char *buf)
     case COC_IPV6_ADDR:
       if (addr->sa_family == AF_INET6)
 	{
-	  return (IN6_ARE_ADDR_EQUAL (&e->addr.ipv6, INETX_ADDR (addr)) &&
+	  return (IN6_ARE_ADDR_EQUAL (&e->addr.ipv6, INET6_ADDR (addr)) &&
 		  (!e->port || e->port == INETX_PORT (addr)));
 	}
       else if (addr->sa_family == AF_INET)
@@ -962,7 +961,7 @@ coc_rule_match (coc_entry_t *e, const struct sockaddr *addr, const char *buf)
 	}
       else if (addr->sa_family == AF_INET6)
 	{
-	  if (IN6_IS_ADDR_V4MAPPED (INETX_ADDR (addr)))
+	  if (IN6_IS_ADDR_V4MAPPED (INET6_ADDR (addr)))
 	    {
 	      /* TODO */
 	    }
