eb00d55aed07b30a752f54b8cc36e387d9dcbde0
Thomas Girard <thomas.g.girard@free.fr>
2017-09-05 00:02:29 +0200

    Initial version of coc.ps1
    Environment variables are set for the powershell
    process only.

--- coc.ps1
+++ coc.ps1
@@ -1,42 +1,92 @@
-﻿# connect-or-cut -- block unwanted connect() calls.
-#
-# Copyright (c) 2017 Thomas Girard <thomas.g.girard@free.fr>
-#
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#
-#  * Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-#
-#  * Neither the name of the author nor the names of its contributors
-#    may be used to endorse or promote products derived from this software
-#    without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
-# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
-# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
-# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
-# SUCH DAMAGE.
-<#
+﻿<#
 .SYNOPSIS
-coc.ps1 sets appropriate environment variables for connect-or-cut.
+coc configures connect-or-cut for the specified program.
+
+.DESCRIPTION
+coc helper script sets environment variables then invokes coc.exe on the
+specified program. If no program is specified it outputs environment
+variables that need to be set to replicate the requested configuration.
+
 .PARAMETER AllowDNS
 Allow connections to DNS nameservers
+
+.PARAMETER Allow
+List of allowed host. Can be specified as host, host:port or as a glob like
+"*.google.com". Specifying "*" allows every connection.
+
+.PARAMETER Block
+List of blocked hosts. Can be specified as host, host:port or as a glob like
+"*.google.com". Specifying "*" can block every connection.
+
+.PARAMETER Help
+Shows this help message
+
 #>
 [CmdletBinding()]
 param (
-[boolean]$AllowDNS = $false
+[boolean]$AllowDNS = $false,
+[string[]]$Allow = @(),
+[string[]]$Block = @(),
+[switch]$Help,
+[ValidateSet("stderr","syslog","file")]
+[string[]]$LogTarget = @("stderr"),
+[string]$LogPath,
+[ValidateSet("silent","error","block","allow","debug")]
+[string]$LogLevel = "block",
+[switch]$Version,
+[Parameter(ValueFromRemainingArguments=$true)]
+[string[]]$ProgramAndParams
 )
+
+function Split-String([string]$s)
+{
+    if ($s -eq $null) { @() }
+    else { ,$s.Split(";") } # , prevents automatic conversion string[1] => string
+}
+
+function Join-String([string[]]$a)
+{
+    if ($a.Count -eq 0) { $null }
+    else { [string]::Join(";", $a) }
+}
+
+function Append-ProcessEnvironment([string]$name, [string[]]$values)
+{
+    $current = Split-String([Environment]::GetEnvironmentVariable($name, "Process"))
+    [Environment]::SetEnvironmentVariable($name, (Join-String($current + $values)), "Process")
+}
+
+$scriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+$LogTargets = @{"stderr" = 1; "syslog" = 2; "file" = 3}
+$LogLevels = @{"silent" = 0; "error" = 1; "block" = 2; "allow" = 3; "debug" = 4}
+
+if ($Help -eq $True)
+{
+    help $MyInvocation.MyCommand.Definition -Detailed
+    exit 0
+}
+elseif ($Version -eq $True)
+{
+    $lib = Join-Path -Path $scriptPath -ChildPath "connect-or-cut.dll"
+    try
+    {
+        Write-Output "$((Get-Item $lib -ErrorAction Stop).VersionInfo.FileVersion)"
+        exit 0
+    }
+    catch [System.Management.Automation.ItemNotFoundException]
+    {
+        Write-Output "Cannot find connect-or-cut.dll in $scriptPath; aborting" 
+        exit 1
+    }
+}
+
+Write-Verbose "COC_ALLOW was: $env:COC_ALLOW"
+Write-Verbose "COC_BLOCK was: $env:COC_BLOCK"
+
+Append-ProcessEnvironment -name "COC_ALLOW" -values $Allow
+Append-ProcessEnvironment -name "COC_BLOCK" -values $Block
+
+Write-Verbose "COC_ALLOW is now: $env:COC_ALLOW"
+Write-Verbose "COC_BLOCK is now: $env:COC_BLOCK"
+
+
